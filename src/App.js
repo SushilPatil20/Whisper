bcd95e73531d92dc486c526a094e7d3d3e4170cb
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2024-08-20 10:36:58 +0530

    Started Messaging application..

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Imessage from "./components/Imessage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-800">Radhe Krishna</h1>
+      <Imessage />
+    </div>
+  );
+}
+
+export default App;
