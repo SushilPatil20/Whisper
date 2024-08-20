e9057ea54f9edfcfa0ba74ed217a5d9d9ffff2ce
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2024-08-20 10:36:58 +0530

    Building a messaging application.

---
+++ src/components/Imessage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+const Imessage = () => {
+  return (
+    <div>
+      {/* Side bar  */}
+      <Sidebar />
+      {/* Chat  */}
+    </div>
+  );
+};
+
+export default Imessage;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h2>I am the side bar</h2>
+      <div>Header</div>
+      <div>Chats</div>
+    </div>
+  );
+};
+
+export default Sidebar;
